bb918d6fe6714e335a337fcd88793ca49787352a
Denis Klychkov <dklychkov@networkoptix.com>
2023-01-10 23:55:30 -0800

    VMS-37533: Generate a list of the used Conan dependency references
    The generated file is supposed to be saved by the CI and may be used to get certain copies of the
    used dependencies (e.g. to get the debug symbols).
    
    (cherry picked from commit 54e50de0346ff9d876be615b188ae2a61db03eaf)

--- cmake/generate_open_source_config.cmake
+++ cmake/generate_open_source_config.cmake
@@ -1,11 +1,6 @@
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-nx_configure_file(
-    "${open_source_root}/build_info.txt"
-    ${CMAKE_CURRENT_BINARY_DIR}/distrib)
-nx_configure_file(
-    "${open_source_root}/build_info.json"
-    ${CMAKE_CURRENT_BINARY_DIR}/distrib)
-nx_configure_file(
-    "${open_source_root}/nx_log_viewer.html"
-    ${CMAKE_CURRENT_BINARY_DIR} COPYONLY)
+nx_configure_file("${open_source_root}/build_info.txt" ${CMAKE_BINARY_DIR}/distrib)
+nx_configure_file("${open_source_root}/build_info.json" ${CMAKE_BINARY_DIR}/distrib)
+nx_configure_file("${open_source_root}/nx_log_viewer.html" ${CMAKE_BINARY_DIR} COPYONLY)
+nx_configure_file(${CMAKE_BINARY_DIR}/conan_refs.txt ${CMAKE_BINARY_DIR}/distrib COPYONLY)
